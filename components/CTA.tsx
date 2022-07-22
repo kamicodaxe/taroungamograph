0c14145cb06f7e84bf183a930f2d582b612ecd1a
KAMI Loïc <ruvice.kami@gmail.com>
2022-07-22 08:32:04 +0100

    - Header responsive - Our Work responsive - CTA Typo
    TIME: 0.5H

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -6,7 +6,7 @@ const CTA: React.FC<{}> = ({ }) => {
         <section className="py-6 text-gray-800">
             <div className="container mx-auto flex flex-col items-center justify-center p-4 space-y-8 md:p-10 lg:space-y-0 lg:flex-row lg:justify-between">
                 <h1 className="text-3xl font-semibold leading-tight text-center lg:text-left">
-                    Vous souhaitez collaborer moi?
+                    Vous souhaitez collaborer avec moi?
                 </h1>
                 <button className="px-8 py-3 text-lg font-semibold rounded bg-primary text-gray-100">
                     Contactez-moi
